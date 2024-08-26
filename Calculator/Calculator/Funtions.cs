e12219e22d8683d425193e2739caf966f1b38c90
Warid Hasan <60097351+waridhasan195@users.noreply.github.com>
2024-08-26 22:28:09 +0600

    Add files via upload

---
+++ Calculator/Calculator/Funtions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Funtions
+    {
+        public static double add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double Multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
---
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+            Console.WriteLine(Funtions.add(5, 4));
+            Console.ReadKey();  
+
+        }
+    }
+}
